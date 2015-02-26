7ad64f39dc87a2ee1e65cf26d890075fb17b36e8
Neil Rackett <neil.rackett@mesmotronic.com>
2015-02-26 22:40:06 +0000

    Added Android-ARM and Android-x86 to build script

--- build/build.cmd
+++ build/build.cmd
@@ -1,11 +1,12 @@
 @echo off
 
+copy ..\android\bin\aircast-jar.jar .\android\
+
 adt ^
  -package ^
  -target ane ../example/libs/AirCast.ane extension.xml ^
  -swc ../swc/bin/aircast-lib.swc ^
+ -platform Android-ARM -C android . ^
+ -platform Android-x86 -C android . ^
  -platform iPhone-ARM -C ios . ^
  -platform default -C default . 
-
-REM -platform Android-ARM -C android . ^
-REM -platform Android-x86 -C android . ^
